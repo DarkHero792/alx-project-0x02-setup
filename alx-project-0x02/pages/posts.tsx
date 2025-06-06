e2d90e7b9551062f47a5f8798054098c6fc3cb2b
Abdelhamid_Marmar <abdelhamidmarmar4@gmail.com>
2025-06-07 00:22:56 +0100

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,18 +1,13 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 import Header from '@/components/layout/Header';
 import PostCard from '@/components/common/PostCard';
 import { type PostProps } from '@/interfaces';
 
-export default function PostsPage() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }, []);
-
+export default function PostsPage({ posts }: PostsPageProps) {
   return (
     <>
       <Head>
@@ -35,4 +30,16 @@ export default function PostsPage() {
       </main>
     </>
   );
+}
+
+// ✅ Static generation using getStaticProps
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+  const posts: PostProps[] = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
