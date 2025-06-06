5f5734f3d364a57b5c4eda5eed478a75668563b1
Abdelhamid_Marmar <abdelhamidmarmar4@gmail.com>
2025-06-06 21:51:57 +0100

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from '@/components/layout/Header';
+import Button from '@/components/common/Button';
 
 export default function AboutPage() {
   return (
@@ -8,8 +9,14 @@ export default function AboutPage() {
         <title>About Page</title>
       </Head>
       <Header />
-      <main className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-50">
+      <main className="flex flex-col items-center gap-4 min-h-screen bg-gray-50 p-6">
         <h1 className="text-4xl font-bold text-purple-600">About This Project</h1>
+
+        <div className="flex gap-4 flex-wrap">
+          <Button label="Small Rounded" size="small" shape="rounded-sm" />
+          <Button label="Medium Button" size="medium" shape="rounded-md" />
+          <Button label="Large Full" size="large" shape="rounded-full" />
+        </div>
       </main>
     </>
   );
