94c4df04230f6466328d8d11edff11d5139f234e
Abdelhamid_Marmar <abdelhamidmarmar4@gmail.com>
2025-06-06 23:52:31 +0100

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,11 +2,20 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white p-4 shadow-md">
-      <nav className="flex space-x-6">
-        <Link href="/" className="hover:underline">Welcome</Link>
-        <Link href="/home" className="hover:underline">Home</Link>
-        <Link href="/about" className="hover:underline">About</Link>
+    <header className="bg-blue-600 text-white py-4 px-6 shadow-md">
+      <nav className="flex gap-6">
+        <Link href="/" className="hover:underline">
+          Welcome
+        </Link>
+        <Link href="/home" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+        <Link href="/posts" className="hover:underline">
+          Posts
+        </Link>
       </nav>
     </header>
   );
